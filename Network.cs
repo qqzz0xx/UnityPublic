0c687ffeba34e2b78880377a81091710fb3f75f5
sqz <632574418@qq.com>
2017-05-18 16:59:28 +0800

    + Network

---
+++ Network.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+enum NetState
+{
+    CLOSED,
+    CONNECTING,
+    CONNECTED,
+    DISCONNECTED,
+    TIMEOUT,
+    ERROR,
+}
+
+
+class Network : IDisposable
+{
+    public event Action<NetState> NetStateChangeListener;
+    public event Action<int, object> MsgResListener;
+
+    private Socket m_Socket;
+    private string m_IP;
+    private int m_Port;
+    private NetState m_NeState;
+    private ManualResetEvent timeoutEvent = new ManualResetEvent(false);
+    private int timeoutMSec = 8000;    //connect timeout count in millisecond
+
+    private Transports m_Transports;
+
+    public string IP
+    {
+        get;
+    }
+
+    public int Port
+    {
+        get;
+    }
+
+    public Network(string server, int port)
+    {
+    }
+
+    public void Init(string server, int port)
+    {
+        m_IP = server;
+        m_Port = port;
+
+    }
+
+    public void Connet()
+    {
+        timeoutEvent.Reset();
+        OnChangeNetState(NetState.CONNECTING);
+
+        IPHostEntry hostEntry = Dns.GetHostEntry(m_IP);
+        IPAddress ipAddress = null;
+        try
+        {
+            foreach (IPAddress address in hostEntry.AddressList)
+            {
+                if (address.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    ipAddress = address;
+                    break;
+                }
+            }
+        }
+        catch (System.Exception ex)
+        {
+            OnChangeNetState(NetState.ERROR);
+        }
+        IPEndPoint ie = new IPEndPoint(ipAddress, m_Port);
+        m_Socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        m_Socket.BeginConnect(ie, new AsyncCallback((result) =>
+        {
+
+            try
+            {
+                m_Socket.EndConnect(result);
+                m_Transports = new Transports(m_Socket, Disconnet, Dispatch);
+                OnChangeNetState(NetState.CONNECTED);
+                StartRecv();
+            }
+            catch (System.Exception ex)
+            {
+                OnChangeNetState(NetState.ERROR);
+                Dispose();
+            }
+            finally
+            {
+                timeoutEvent.Set();
+            }
+
+
+        }), m_Socket);
+
+        if (timeoutEvent.WaitOne(timeoutMSec, false))
+        {
+            if (m_NeState == NetState.CONNECTING)
+            {
+                OnChangeNetState(NetState.TIMEOUT);
+                Dispose();
+            }
+        }
+    }
+
+    public void StartRecv()
+    {
+        if (m_NeState != NetState.DISCONNECTED) return;
+        m_Transports.Recv();
+    }
+    public void Send(int msgid, byte[] data)
+    {
+        if (m_NeState != NetState.DISCONNECTED) return;
+        m_Transports.Send(msgid, data);
+    }
+
+    private void Dispatch(int msgid, byte[] body)
+    {
+        if (MsgResListener != null)
+            MsgResListener.Invoke(msgid, body);
+    }
+
+    public void Disconnet()
+    {
+        OnChangeNetState(NetState.DISCONNECTED);
+        Dispose();
+    }
+
+    private void OnChangeNetState(NetState state)
+    {
+        m_NeState = state;
+        if (NetStateChangeListener != null)
+        {
+            NetStateChangeListener.Invoke(state);
+        }
+    }
+
+    #region IDisposable Support
+    private bool disposedValue = false; // 要检测冗余调用
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!disposedValue)
+        {
+            if (disposing)
+            {
+                // TODO: 释放托管状态(托管对象)。
+                try
+                {
+                    m_Transports.Close();
+                    m_Socket.Shutdown(SocketShutdown.Both);
+                    m_Socket.Close();
+                    m_Socket = null;
+                }
+                catch (Exception)
+                {
+                    //todo : 有待确定这里是否会出现异常，这里是参考之前官方github上pull request。emptyMsg
+                }
+            }
+
+            // TODO: 释放未托管的资源(未托管的对象)并在以下内容中替代终结器。
+            // TODO: 将大型字段设置为 null。
+
+            disposedValue = true;
+        }
+    }
+
+    // TODO: 仅当以上 Dispose(bool disposing) 拥有用于释放未托管资源的代码时才替代终结器。
+    // ~Network() {
+    //   // 请勿更改此代码。将清理代码放入以上 Dispose(bool disposing) 中。
+    //   Dispose(false);
+    // }
+
+    // 添加此代码以正确实现可处置模式。
+    public void Dispose()
+    {
+        // 请勿更改此代码。将清理代码放入以上 Dispose(bool disposing) 中。
+        Dispose(true);
+        // TODO: 如果在以上内容中替代了终结器，则取消注释以下行。
+        // GC.SuppressFinalize(this);
+    }
+    #endregion
+}
+
---
+++ Transports.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+
+class StateObject
+{
+    public const int BufferSize = 1024;
+    public byte[] Buffer = new byte[BufferSize];
+}
+
+public class Transports
+{
+    enum TransportState
+    {
+        eClose,
+        ePaserHead,
+        ePaserBody,
+    }
+
+    //依赖注入
+    private Action onDisconnect = null;
+    private Action<int, byte[]> onDispatch = null;
+    private Socket m_Socket;
+
+    //
+    private StateObject m_StateObject = new StateObject();
+    private TransportState m_TransportState;
+    private bool m_isOnSending = false;
+    private bool m_isOnReceiving = false;
+
+    //proto
+    private const int s_MsgHeadLen = 6;
+    private byte[] m_MsgHead = new byte[s_MsgHeadLen];
+    private byte[] m_MsgBody;
+    private int m_iHadCopyHead;
+    private int m_iHadCopyBody;
+
+    private int m_iMsgID;
+    private int m_iSeqID;
+    private int m_iMsgBodyLen;
+
+    public Transports(Socket socket, Action onDisconnet, Action<int, byte[]> onDispatch)
+    {
+        this.m_Socket = socket;
+        this.onDisconnect = onDisconnet;
+        this.onDispatch = onDispatch;
+        m_TransportState = TransportState.ePaserHead;
+    }
+    public void Send(int msgid, byte[] body)
+    {
+        if (m_TransportState == TransportState.eClose) return;
+        m_isOnSending = true;
+        byte[] buffer = new byte[body.Length + 4];
+        buffer[0] = Convert.ToByte(msgid & 0xFF);
+        buffer[1] = Convert.ToByte(msgid >> 8 & 0xFF);
+        buffer[2] = Convert.ToByte(body.Length & 0xFF);
+        buffer[3] = Convert.ToByte(body.Length >> 8 & 0xFF);
+        WriteBytes(body, 0, buffer, 4, body.Length);
+        m_Socket.BeginSend(buffer, 0, buffer.Length, SocketFlags.None, new AsyncCallback(SendCallback), m_StateObject);
+    }
+    private void SendCallback(IAsyncResult result)
+    {
+        if (m_TransportState == TransportState.eClose) return;
+        m_Socket.EndSend(result);
+        m_isOnSending = false;
+    }
+    public void Recv()
+    {
+        m_isOnReceiving = true;
+        m_Socket.BeginReceive(m_StateObject.Buffer, 0, m_StateObject.Buffer.Length, SocketFlags.None, 
+            new AsyncCallback(RecvCallback), m_StateObject);
+    }
+    private void RecvCallback(IAsyncResult reslut)
+    {
+        if (m_TransportState == TransportState.eClose) return;
+
+        StateObject state = (StateObject)reslut.AsyncState;
+        Socket socket = m_Socket;
+
+        try
+        {
+            int length = socket.EndReceive(reslut);
+            m_isOnReceiving = false;
+            if (length > 0)
+            {
+                ProBytes(state.Buffer, 0, length);
+
+                if (m_TransportState != TransportState.eClose)
+                    Recv();
+            }
+            else
+            {
+                if (onDisconnect != null) onDisconnect();
+            }
+        }
+        catch (System.Exception ex)
+        {
+            if (onDisconnect != null) onDisconnect();
+        }
+    }
+
+    private void ProBytes(byte[] buffer, int offset, int length)
+    {
+        if (buffer == null || length <= 0 || offset >= length) return;
+        int iDataLen = length - offset;
+        while (iDataLen > 0)
+        {
+            if (m_TransportState == TransportState.ePaserHead)
+            {
+                int iCopyHead = s_MsgHeadLen - m_iHadCopyHead;
+                if (iDataLen >= iCopyHead)
+                {
+                    WriteBytes(buffer, offset, m_MsgHead, m_iHadCopyHead, iCopyHead);
+                    offset += iCopyHead;
+                    iDataLen -= iCopyHead;
+                    m_iHadCopyHead = 0;
+
+                    m_iMsgID = m_MsgHead[0] + (m_MsgHead[1] << 8);
+                    m_iSeqID = m_MsgHead[2] + (m_MsgHead[3] << 8);
+                    m_iMsgBodyLen = m_MsgHead[4] + (m_MsgHead[5] << 8);
+
+                    if (m_iMsgBodyLen > 0)
+                    {
+                        m_iHadCopyBody = 0;
+                        m_MsgBody = new byte[m_iMsgBodyLen];
+                        m_TransportState = TransportState.ePaserBody;
+                    }
+                    else
+                    {
+                        if (onDispatch != null)
+                        {
+                            onDispatch(m_iMsgID, m_MsgBody);
+                        }
+                        ResetPaserState();
+                    }
+                }
+                else
+                {
+                    WriteBytes(buffer, offset, m_MsgHead, m_iHadCopyHead, iDataLen);
+                    m_iHadCopyHead += iDataLen;
+                    return;
+                }
+            }
+            else if (m_TransportState == TransportState.ePaserBody)
+            {
+                int iCopyBody = m_iMsgBodyLen - m_iHadCopyBody;
+                if (iDataLen >= iCopyBody)
+                {
+                    WriteBytes(buffer, offset, m_MsgBody, m_iHadCopyBody, iCopyBody);
+                    offset += iCopyBody;
+                    iDataLen -= iCopyBody;
+                    if (onDispatch != null)
+                    {
+                        onDispatch(m_iMsgID, m_MsgBody);
+                    }
+                    ResetPaserState();
+                }
+                else
+                {
+                    WriteBytes(buffer, offset, m_MsgBody, m_iHadCopyBody, iDataLen);
+                    m_iHadCopyBody += iDataLen;
+                    return;
+                }
+
+            }
+        }
+    }
+    private void ResetPaserState()
+    {
+        m_TransportState = TransportState.ePaserHead;
+        m_iHadCopyBody = 0;
+        m_iHadCopyHead = 0;
+    }
+    private void WriteBytes(byte[] src, int srcOffset, byte[] dst, int dstOffset, int length)
+    {
+        Buffer.BlockCopy(src, srcOffset, dst, dstOffset, length);
+    }
+
+    public void Close()
+    {
+        m_TransportState = TransportState.eClose;
+    }
+}
